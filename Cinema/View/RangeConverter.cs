e16befef55a120a1c7b6d0ec741e150c2ea4d157
“anna.moskalenko” <anna.moskalenko@eleks.com>
2021-12-18 19:40:46 +0200

    depending on the rating change the color of movieName

---
+++ Cinema/View/RangeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Cinema.VM
+{
+    internal class RangeConverter : IValueConverter
+    {
+        public object Convert(
+        object value, Type targetType,
+        object parameter, CultureInfo culture)
+        {
+            double number = (double)System.Convert.ChangeType(value, typeof(double));
+
+            if (number < 5)
+                return -1;
+
+            if (number >= 5 && number < 7.5)
+                return 0;
+
+            return +1;
+        }
+
+        public object ConvertBack(
+            object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack not supported");
+        }
+    }
+}
