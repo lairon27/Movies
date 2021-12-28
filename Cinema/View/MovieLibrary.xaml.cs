d85970c93f943738c60b2ae37b4ef584b184fd85
“anna.moskalenko” <anna.moskalenko@eleks.com>
2021-12-28 23:27:16 +0200

    added Combobox depending on the selected element in the Combobox changes the font of the text and the size of the image

--- Cinema/View/MovieLibrary.xaml.cs
+++ Cinema/View/MovieLibrary.xaml.cs
@@ -1,22 +1,5 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Data.Entity;
-using Cinema.Model;
-using Cinema.VM;
-using Cinema.View;
-using Cinema.Dialog;
 
 namespace Cinema.View
 {
@@ -28,14 +11,6 @@ namespace Cinema.View
         public MovieLibrary()
         {
             InitializeComponent();
-            DataContext = new MovieLibraryVM();
-
-            //this.Loaded += MovieLibrary_Loaded;
         }
-
-        //private void MovieLibrary_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    searchTextBox.Focus();
-        //}
     }
 }
