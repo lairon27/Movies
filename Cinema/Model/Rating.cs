ebb6457e0ceab07b1e16db2ee4dcd638e4d3cd7d
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-01-13 10:18:23 +0200

    added model "Rating"

---
+++ Cinema/Model/Rating.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Model
+{
+    public class Rating : ModelBaseNotify
+    {
+        public Guid movieId;
+        public Guid userId;
+        public int userRating;
+
+        public Guid MovieId
+        {
+            get { return movieId; }
+            set
+            {
+                movieId = value;
+                OnPropertyChanged("Rating");
+            }
+        }
+
+        public Guid UserId
+        {
+            get { return userId; }
+            set
+            {
+                userId = value;
+                OnPropertyChanged("Rating");
+            }
+        }
+
+        public int UserRating
+        {
+            get { return userRating; }
+            set
+            {
+                userRating = value;
+                OnPropertyChanged("Rating");
+            }
+        }
+    }
+}
