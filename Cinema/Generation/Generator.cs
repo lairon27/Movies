d040eb7e1b46d39f2530c67b544b4cde250189cf
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-01-15 00:32:20 +0200

    generating random rating(first try)

--- Cinema/Generation/Generator.cs
+++ Cinema/Generation/Generator.cs
@@ -13,18 +13,37 @@ namespace Cinema.Generation
         public static User GenerateUser()
         {
             User user = new();
-            Movie movie = new();
+            //var rating = GenerateRating();
 
-            var generatedUserFaker = new Faker<User>()
+            return user = new Faker<User>()
                .RuleFor(x => x.UserId, f => Guid.NewGuid())
                .RuleFor(x => x.UserName, f => f.Name.FirstName())
                .RuleFor(x => x.UserLastName, f => f.Name.LastName())
                .RuleFor(x => x.BirthDate, f => f.Date.Between(new DateTime(1970, 1, 1), new DateTime(2010, 1, 1)).ToString("d"))
-               .RuleFor(x => x.AmountOfRatedFilms, f => f.Random.Int(0, 11));
+               .RuleFor(x => x.AmountOfRatedFilms, f => f.Random.Int(0, 11))
+               .Generate();
+        }
 
-            user = generatedUserFaker.Generate();
+        public static Rating GenerateRating()
+        {
+            Rating rating = new ();
 
-            return user;
+            Random random = new();
+            MovieLibraryVM movie = new();
+
+            var moviesId = new List<Guid>();
+
+            foreach (var item in movie.Movies)
+            {
+                moviesId.Add(item.MovieId);
+            }
+
+            int index = random.Next(moviesId.Count);
+
+            rating.MovieId = moviesId[index];
+            rating.UserRating = random.Next(0, 10);
+
+            return rating;
         }
     }
 }
