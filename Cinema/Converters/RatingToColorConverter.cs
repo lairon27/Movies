615e621278e668afd5308bf171843873e7dcd1fc
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-01-17 19:01:47 +0200

    added variable "color" which return color for movie title

--- Cinema/Converters/RatingToColorConverter.cs
+++ Cinema/Converters/RatingToColorConverter.cs
@@ -11,13 +11,15 @@ namespace Cinema.Converters
         {
             double number = (double)System.Convert.ChangeType(value, typeof(double));
 
+            SolidColorBrush color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4287f5"));
+
             if (number < 5)
-                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff6161"));
+                color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff6161"));
 
             if (number >= 5 && number < 7.5)
-                return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffc052"));
+                color = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffc052"));
 
-            return new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4287f5"));
+            return color;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
