75697ae1d1a6f200b5077ed0599d155e99d21441
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-01-19 22:44:45 +0200

    added StackPanel for displaying Rating

--- Cinema/Generation/Generator.cs
+++ Cinema/Generation/Generator.cs
@@ -2,32 +2,50 @@
 using Cinema.Model;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Windows;
 
 namespace Cinema.Generation
 {
     internal class Generator
     {
-        public static User GenerateUser()
-        {
-            User user = new();
-            //var rating = GenerateRating();
+        public static Generator faker;
 
-            return user = new Faker<User>()
+        private Generator() { }
+
+        public static Generator GetInstance
+        {
+            get
+            {
+                if (faker == null)
+                    faker = new Generator();
+                return faker;
+            }
+        }
+
+        public User GenerateUser()
+        {
+            Random random = new();
+
+            var rating = new List<Rating>();
+
+            for(var i=0; i<100; i++)
+            {
+                rating.Add(GenerateRating());
+            }
+
+            return new Faker<User>()
                .RuleFor(x => x.UserId, f => Guid.NewGuid())
                .RuleFor(x => x.UserName, f => f.Name.FirstName())
                .RuleFor(x => x.UserLastName, f => f.Name.LastName())
                .RuleFor(x => x.BirthDate, f => f.Date.Between(new DateTime(1970, 1, 1), new DateTime(2010, 1, 1)).ToString("d"))
                .RuleFor(x => x.AmountOfRatedFilms, f => f.Random.Int(0, 11))
+               .RuleFor(x => x.Ratings, (f,o) => Enumerable.Range(1, o.AmountOfRatedFilms).Select(r => f.PickRandom(rating)).ToList())
                .Generate();
         }
 
         public static Rating GenerateRating()
         {
             Rating rating = new ();
-
             Random random = new();
             MovieLibraryVM movie = new();
 
