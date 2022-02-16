c0d9611954a30eade1d00215b89e1369265db147
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-02-17 01:26:41 +0200

    Created converters for converting id to name;

---
+++ Cinema/Converters/MovieIdToTitleConverter.cs
@@ -0,0 +1,31 @@
+﻿using Cinema.Service;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Cinema.Converters
+{
+    internal class MovieIdToTitleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Guid id = (Guid)values[0];
+
+            string name = id.ToString();
+
+            if(values[1] != DependencyProperty.UnsetValue)
+            {
+                DataManager manager = (DataManager)values[1];
+                name = manager.GetMovieById(id).MovieName;
+            }
+ 
+            return name;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cinema/Converters/RatingToStarsConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cinema.Converters
+{
+    internal class RatingToStarsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double number = (double)System.Convert.ChangeType(value, typeof(double));
+
+            var amountOfStars = 5;
+
+            if (number <= 2)
+            {
+                amountOfStars = 1;
+            }
+
+            if (number > 2 && number <= 4)
+            {
+                amountOfStars = 2;
+            }
+
+            if (number > 4 && number <= 6)
+            {
+                amountOfStars = 3;
+            }
+
+            if (number > 6 && number <= 8)
+            {
+                amountOfStars = 4;
+            }
+
+            return amountOfStars;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
