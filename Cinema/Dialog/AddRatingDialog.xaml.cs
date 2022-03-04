62ea85e09731e1e901a90cf48589198e559b01bf
“anna.moskalenko” <anna.moskalenko@eleks.com>
2022-03-04 19:15:55 +0200

    added dialog for showing ratings and dialogs for add rating for movie

---
+++ Cinema/Dialog/AddRatingDialog.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Cinema.Model;
+using Cinema.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema.Dialog
+{
+    /// <summary>
+    /// Interaction logic for AddRatingDialog.xaml
+    /// </summary>
+    public partial class AddRatingDialog : Window
+    {
+        private string title;
+        private int rating;
+
+        public AddRatingDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        public string MovieTitle
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+    }
+}
---
+++ Cinema/Dialog/RatingsByUsers.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Cinema.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema.Dialog
+{
+    /// <summary>
+    /// Interaction logic for RatingsByUsers.xaml
+    /// </summary>
+    public partial class RatingsByUsers : Window
+    {
+        public static readonly DependencyProperty DataManagerProperty =
+         DependencyProperty.Register("DataManager", typeof(IDataManager), typeof(RatingsByUsers), new
+            PropertyMetadata(default(IDataManager)));
+
+        public IDataManager DataManager
+        {
+            get { return (IDataManager)GetValue(DataManagerProperty); }
+            set { SetValue(DataManagerProperty, value); }
+        }
+
+        public RatingsByUsers(Movie movie)
+        {
+            InitializeComponent();
+            DataContext = movie;
+        }
+    }
+}
